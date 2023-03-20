ae64768717573155c3da390ee5847c4b8a09f69a
Nathan Hibbert <nathanhibbert96@gmail.com>
2023-03-20 14:25:42 +0000

    updates complete

--- models/product.model.js
+++ models/product.model.js
@@ -8,8 +8,7 @@ class Product {
         this.price = +productData.price;
         this.description = productData.description;
         this.image = productData.image; // the name of the image file
-        this.imagePath = `product-data/images/${productData.image}`;
-        this.imageUrl = `/products/assets/images/${productData.image}`;
+        this.updateImageData();
         if (productData._id) {
             this.id = productData._id.toString();
         }
@@ -34,7 +33,7 @@ class Product {
             error.code = 404;
             throw error;
         }
-        return product;
+        return new Product(product);
     }
 
     static async findAll() {
@@ -49,6 +48,11 @@ class Product {
         });
     }
 
+    updateImageData() {
+        this.imagePath = `product-data/images/${this.image}`;
+        this.imageUrl = `/products/assets/images/${this.image}`;
+    }
+
     async save() {
         const productData = {
             title: this.title,
@@ -57,7 +61,29 @@ class Product {
             description: this.description,
             image: this.image,
         };
-        await db.getDb().collection("products").insertOne(productData);
+
+        if (this.id) {
+            const productId = new mongodb.ObjectId(this.id);
+
+            if (!this.image) {
+                delete productData.image;
+            }
+
+            await db.getDb().collection("products").updateOne(
+                { _id: productId },
+                {
+                    $set: productData,
+                }
+            );
+        } else {
+            await db.getDb().collection("products").insertOne(productData);
+        }
+
+        replaceImage(newImage);
+        {
+            this.image = newImage;
+            this.updateImageData();
+        }
     }
 }
 
