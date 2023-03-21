f500d47da83db20e40a70467afe3e2f923bad7a1
Nathan Hibbert <nathanhibbert96@gmail.com>
2023-03-21 03:28:24 +0000

    all done

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -6,4 +6,12 @@ router.get("/", function (req, res) {
     res.redirect("/products");
 });
 
+router.get("401", function (req, res) {
+    res.status(401)("shared/401");
+});
+
+router.get("403", function (req, res) {
+    res.status(403)("shared/403");
+});
+
 module.exports = router;
